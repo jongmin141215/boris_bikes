04ccc88eecd97e6fc97970b65245c392fab54d01
Jongmin Kim <kjm141215@gmail.com>
2015-09-02 12:03:54 +0100

    Passed second unit test(:release_bike)

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,10 @@
 require 'docking_station'
 
 describe DockingStation do
+  # it 'responds to the release_bike method' do
+  #   expect(subject).to respond_to(:release_bike)
+  # end
+  it { is_expected.to respond_to(:release_bike) }
+
+
 end
