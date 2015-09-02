5d0d0260792fa3949f0db338775cc0a783038d19
Jongmin Kim <kjm141215@gmail.com>
2015-09-02 18:46:05 +0100

    Added DEFAULT_CAPACITY

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,16 +1,30 @@
 class DockingStation
+  attr_accessor :capacity
+  DEFAULT_CAPACITY = 20
+
   def initialize
     @bikes = []
+    @capacity = DEFAULT_CAPACITY
   end
 
   def release_bike
     # Bike.new
-    fail 'No bikes available' if @bikes == [] 
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-  	fail 'Docking station full' if @bikes.count >= 20
+  	fail 'Docking station full' if full?
   	@bikes << bike
+  end
+
+  private
+
+  def full?
+    @bikes.count >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    @bikes == []
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -29,6 +29,10 @@ describe DockingStation do
       end
   end
 
+  it 'has a default capacity' do
+    expect(subject.capacity).to eq(DockingStation::DEFAULT_CAPACITY)
+  end
+
 
     # expect(subject.release_bike.working?).to be true
 
