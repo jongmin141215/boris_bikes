e08cf6031f10da33fee57fe1f746c4f83409d3ab
danielstpaul <danielstpaul@hotmail.com>
2015-09-04 12:24:10 +0100

    updated docking_station_spec.rb to implement 'London Style' TDD

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,8 +8,8 @@ describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
   #it { expect(subject.release_bike).to be_working }
   it 'releases a working bike' do
-      subject.dock Bike.new
-      my_bike = subject.release_bike
+      my_bike = double :bike, working?: true
+      subject.dock my_bike
       expect(my_bike).to be_working
   end
 
@@ -20,8 +20,7 @@ describe DockingStation do
   end
 
   it 'raises an error when there are no working bikes' do
-    bike = Bike.new
-    bike.report_broken
+    bike = double :bike, working?: false
     subject.dock(bike)
     expect{subject.release_bike}.to raise_error 'No bikes available'
   end
@@ -30,9 +29,9 @@ describe DockingStation do
       # dock_1 = DockingStation.new
       # 20.times {dock_1.dock Bike.new}
       # it do expect {dock_1.dock Bike.new}.to raise_error 'Docking station full' end
-      it do
-        subject.capacity.times {subject.dock(Bike.new)}
-        expect{subject.dock(Bike.new)}.to raise_error 'Docking station full'
+      it 'cannot dock a bike when docking station if full' do
+        subject.capacity.times {subject.dock :bike}
+        expect{subject.dock :bike}.to raise_error 'Docking station full'
       end
   end
 
